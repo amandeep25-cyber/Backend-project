ddbf6400403af25668f1de0d81bbc3cc6d7084cd
amandeep25 <st.amandeep25@gmail.com>
2024-12-04 21:00:47 +0530

    Database connected successfully

---
+++ src/db/database.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async ()=>{
+     try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log("Database connected Successfully and host:"+ connectionInstance.connection.host)
+     } catch (error) {
+        console.error("MongoDB connection FAILED:" + error)
+     }
+}
+
+export default connectDB
