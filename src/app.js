c969992c75284370cdc55593a27260bf2d1c2def
amandeep25 <st.amandeep25@gmail.com>
2024-12-23 22:24:54 +0530

    register router and controller with debugging

--- src/app.js
+++ src/app.js
@@ -5,17 +5,24 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
+app.use( cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
-app.use(express.json({limit:"16kb"}));  // jo bhi data form se request me aata hai
-app.use(express.urlencoded({extended,limit:"16kb"})); // jo bhi data URL se aata hai
+app.use(express.json({ limit: "16kb" }));  // jo bhi data form se request me aata hai
+app.use(express.urlencoded({ extended: true , limit: "16kb" })); // jo bhi data URL se aata hai
 
 app.use(express.static("public")) // store the file in static folder such as "public"
 
 app.use(cookieParser()) // using cookie-parser 
+
+
+// router import
+import registerRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users",registerRouter)
 
 
 export { app }
