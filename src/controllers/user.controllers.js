99f1cc0f5f6989e4dd9149164c6dbf3072eb859b
amandeep25 <st.amandeep25@gmail.com>
2024-12-24 22:38:36 +0530

    logic building in user register of controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,7 +1,65 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { User } from "../models/user.models.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
 const registerUser = asyncHandler( async (req,res)=>{
-    res.status(200).json({"message":"ok"})
+    
+    // get data from user
+    const {fullName,email,username,password} = req.body;
+    
+    // check for validation - not empty
+    if(
+        [fullName,email,username,password].some((field)=>
+        field?.trim()==="")
+    ){
+        throw new ApiError(400,"Any field should not be empty"); 
+    }
+
+    // check for Existed user
+    const existedUser = await User.findOne({
+        $or: [{username},{email}]
+    })
+
+    if(existedUser){
+        throw new ApiError(409,"User already existed")
+    }
+
+    // check for images,check for avatar
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar image is required")
+    }
+    
+    // upload them on cloudinary
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    const user = await User.create({
+        fullName,
+        email,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        password,
+        username: username.toLowerCase(),
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    
+    if(!createdUser){
+        throw new ApiError(500 ,"Something went wrong while creating user")
+    }
+
+    // send response to user
+    return res.status(201).json(
+        new ApiResponse(200 , createdUser)
+    )
+
 })
 
 export { registerUser }
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -34,7 +34,7 @@ const userSchema = mongoose.Schema(
         },
         watchHistory: [
             {
-                type: Schema.Types.ObjectId,
+                type: mongoose.Schema.Types.ObjectId,
                 ref: "Video"
             }
         ],
